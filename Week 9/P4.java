fa90d1efd7628b13f01675f4db8faf90132f91ba
Rituraj Gupta <rituraj.iter@gmail.com>
2021-01-07 14:39:27 +0530

    Only for odd condition.

--- Week 9/P4.java
+++ Week 9/P4.java
@@ -1,31 +1,39 @@
 import java.util.*;
 
 public class P4 {
+	public static void printvalue(int n) {
+		int a = (n / 2);
+		for (int i = 1; i <= n - 1; i++) {
+			System.out.print(a);
+			if (i <= (n / 2) - 1) {
+				a--;
+			} else {
+				a++;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number of column");
-		int n = sc.nextInt(), num = 2, a = n / 2;
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= a; j++) {
-				System.out.print(" ");
-			}
-			int ilp = (num / 2);
-			for (int k = 1; k <= num - 1; k++) {
-				System.out.print(ilp);
-				if (k <= (num / 2) - 1) {
-					ilp--;
-				} else {
-					ilp++;
+		System.out.println("Enter number of column odd only");
+		int n = sc.nextInt(), a = 2, b = n / 2;
+		if (n % 2 != 0) {
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= b; j++) {
+					System.out.print(" ");
 				}
+				printvalue(a);
+				if (i <= n / 2) {
+					--b;
+					a += 2;
+				} else {
+					++b;
+					a -= 2;
+				}
+				System.out.println();
 			}
-			if (i <= n / 2) {
-				--a;
-				num += 2;
-			} else {
-				++a;
-				num -= 2;
-			}
-			System.out.println();
+		} else {
+			System.out.println("Even numbers of colums are enterd");
 		}
 	}
 }
